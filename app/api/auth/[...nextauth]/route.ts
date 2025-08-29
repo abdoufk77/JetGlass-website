7fa3f2904c6d2f794858cf8256d8245f2e630095
abdoufk77 <ab.foukahy@gmail.com>
2025-08-29 14:53:05 +0100

    try to fix the same problem

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,8 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
+export const runtime = 'nodejs'
+
 import NextAuth from 'next-auth'
 import { authOptions } from '@/lib/auth'
 
